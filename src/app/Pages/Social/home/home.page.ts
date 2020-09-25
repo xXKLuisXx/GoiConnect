40dd7a3d225570e6f2c03f72e2a4a48fc3ebf106
luis <proffine1@gmail.com>
2020-09-25 15:22:31 -0500

    commit camera

--- src/app/Pages/Social/home/home.page.ts
+++ src/app/Pages/Social/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private camera: Camera,
+    public actionSheetController: ActionSheetController
+  ) { }
 
   ngOnInit() {
   }
 
-  loadData(){
-    console.log('Datos');
+  croppedImagepath = "";
+  isLoading = false;
+
+  imagePickerOptions = {
+    maximumImagesCount: 1,
+    quality: 50
+  };
+
+  
+
+
+  pickImage(sourceType) {
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: sourceType,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      this.croppedImagepath = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error 
+    });
+  }
+
+  async selectImage() {
+    const actionSheet = await this.actionSheetController.create({
+      header: "Select Image source",
+      buttons: [{
+        text: 'Load from Library',
+        handler: () => {
+          this.pickImage(this.camera.PictureSourceType.PHOTOLIBRARY);
+        }
+      },
+      {
+        text: 'Use Camera',
+        handler: () => {
+          this.pickImage(this.camera.PictureSourceType.CAMERA);
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      }
+      ]
+    });
+    await actionSheet.present();
   }
 
 }
--- src/app/Principal/Social/social-home/social-home.page.ts
+++ src/app/Principal/Social/social-home/social-home.page.ts
@@ -5,6 +5,9 @@ import { MarketPage } from 'src/app/Pages/Social/market/market.page';
 import { StoriesPage } from 'src/app/Pages/Social/stories/stories.page';
 import { ChatPage } from 'src/app/Pages/Social/chat/chat.page';
 import { ProfilePage } from 'src/app/Pages/Social/profile/profile.page';
+import { SuperTabChangeEventDetail } from '@ionic-super-tabs/core';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-social-home',
@@ -18,9 +21,23 @@ export class SocialHomePage implements OnInit {
   chatPage = ChatPage;
   profilePage = ProfilePage;
   menuAppsPage = MenuAppsPage;
-  constructor() { }
+  constructor(public navCtrl:NavController) { }
 
   ngOnInit() {
+  }
+
+  OnTabChange(ev: CustomEvent<SuperTabChangeEventDetail>){
+    switch (ev.detail.index){
+      case 0:
+        // console.log("Estas en apps");
+        break;
+      case 1:
+        // console.log("Estas en social");
+        break;
+      case 2:
+        // console.log("Estas en profesional")
+        break;
+    }
   }
 
 }
